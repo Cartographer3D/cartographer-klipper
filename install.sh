e946fc4cd80d515e1d8b02010260ffd2410480ed
Richard Davies <richardjdavies@me.com>
2023-11-12 08:49:47 +0800

    Added support for CAN and lis2dw

--- install.sh
+++ install.sh
@@ -17,24 +17,24 @@ if [ ! -d "$KDIR" ] || [ ! -d "$KENV" ]; then
 fi
 
 # install idm requirements to env
-echo "idm: installing python requirements to env, this may take 10+ minutes."
+echo "Cartographer: installing python requirements to env, this may take 10+ minutes."
 "${KENV}/bin/pip" install -r "${BKDIR}/requirements.txt"
 
 # update link to idm.py
-echo "idm: linking klippy to idm.py."
+echo "Cartographer: linking klippy to the cartographer.py."
 if [ -e "${KDIR}/klippy/extras/idm.py" ]; then
     rm "${KDIR}/klippy/extras/idm.py"
 fi
-if [ -e "${KDIR}/klippy/extras/idm_accel.py" ]; then
-    rm "${KDIR}/klippy/extras/idm_accel.py"
+if [ -e "${KDIR}/klippy/extras/cartographer.py" ]; then
+    rm "${KDIR}/klippy/extras/cartographer.py"
 fi
-ln -s "${BKDIR}/idm.py" "${KDIR}/klippy/extras/idm.py"
-ln -s "${BKDIR}/idm_accel.py" "${KDIR}/klippy/extras/idm_accel.py"
+ln -s "${BKDIR}/cartographer.py" "${KDIR}/klippy/extras/cartographer.py"
+ln -s "${BKDIR}/cartographer.py" "${KDIR}/klippy/extras/cartographer.py"
 # exclude idm.py from klipper git tracking
 if ! grep -q "klippy/extras/idm.py" "${KDIR}/.git/info/exclude"; then
     echo "klippy/extras/idm.py" >> "${KDIR}/.git/info/exclude"
 fi
-if ! grep -q "klippy/extras/idm_accel.py" "${KDIR}/.git/info/exclude"; then
-    echo "klippy/extras/idm_accel.py" >> "${KDIR}/.git/info/exclude"
+if ! grep -q "klippy/extras/cartographer.py" "${KDIR}/.git/info/exclude"; then
+    echo "klippy/extras/cartographer.py" >> "${KDIR}/.git/info/exclude"
 fi
-echo "idm: installation successful."
+echo "Cartographer Probe: installation successful."
