1b1331ab3701a6ac6d525e9f444ff8a2411ba1ab
Richard Davies <richardjdavies@me.com>
2024-02-03 20:40:20 +0800

    Update cartographer.py

--- cartographer.py
+++ cartographer.py
@@ -602,9 +602,9 @@ class CartographerProbe:
     def _get_trapq_position(self, print_time):
         move = None
         if self._last_trapq_move:
-            last = self._last_trapq_move
+            last = self._last_trapq_move[0]
             last_end = last.print_time + last.move_t
-            if last.print_time <= print_time <= last_end:
+            if last.print_time <= print_time < last_end:
                 move = last
         if move is None:
             ffi_main, ffi_lib = chelper.get_ffi()
@@ -612,8 +612,8 @@ class CartographerProbe:
             count = ffi_lib.trapq_extract_old(self.trapq, data, 1, 0.0, print_time)
             if not count:
                 return None, None
+            self._last_trapq_move = data
             move = data[0]
-        self._last_trapq_move = move
         move_time = max(0.0, min(move.move_t, print_time - move.print_time))
         dist = (move.start_v + .5 * move.accel * move_time) * move_time
         pos = (move.start_x + move.x_r * dist, move.start_y + move.y_r * dist,
@@ -889,7 +889,7 @@ class CartographerProbe:
         old_offset = self.model.offset
         self.model.offset += offset
         self.model.save(self, False)
-        gcmd.respond_info("Cartographer model offset has been updated to %.5f\n"
+        gcmd.respond_info(f"Cartographer model offset has been updated to {self.model.offset}.\n"
                 "You must run the SAVE_CONFIG command now to update the\n"
                 "printer config file and restart the printer.")
         self.model.offset = old_offset
@@ -1445,6 +1445,7 @@ class CartographerMeshHelper:
                     "'relative_reference_index' options are specified. The"
                     " former will be used")
 
+        self.faulty_region_ = []
         self.faulty_regions = []
         for i in list(range(1, 100, 1)):
             start = mesh_config.getfloatlist("faulty_region_%d_min" % (i,), None,
@@ -1457,7 +1458,8 @@ class CartographerMeshHelper:
             y_min = min(start[1], end[1])
             y_max = max(start[1], end[1])
             self.faulty_regions.append(Region(x_min, x_max, y_min, y_max))
-        
+            self.faulty_region_.append([x_min, y_min, x_max, y_max])
+        self.faulty_region_ = np.array(self.faulty_region_).T
         self.exclude_object = None
         self.cartographer.printer.register_event_handler(
             "klippy:connect", self._handle_connect
@@ -1587,9 +1589,9 @@ class CartographerMeshHelper:
         return points
 
     def calibrate(self, gcmd):
-        self.min_x, self.min_y = coord_fallback(gcmd, "MESH_MIN", float,
+        self.min_x, self.min_y = coord_fallback(gcmd, "MESH_MIN", convert_float,
                 self.def_min_x, self.def_min_y, lambda v, d: max(v, d))
-        self.max_x, self.max_y = coord_fallback(gcmd, "MESH_MAX", float,
+        self.max_x, self.max_y = coord_fallback(gcmd, "MESH_MAX", convert_float,
                 self.def_max_x, self.def_max_y, lambda v, d: min(v, d))
         self.res_x, self.res_y = coord_fallback(gcmd, "PROBE_COUNT", int,
                 self.def_res_x, self.def_res_y, lambda v, _d: max(v, 3))
@@ -1837,29 +1839,30 @@ class CartographerMeshHelper:
 
     def _interpolate_faulty(self, clusters):
         faulty_indexes = []
-        xi_max = 0
-        yi_max = 0
-        for (xi, yi), points in clusters.items():
-            if xi > xi_max:
-                xi_max = xi
-            if yi > yi_max:
-                yi_max = yi
-            xc = xi * self.step_x + self.min_x
-            yc = yi * self.step_y + self.min_y
-            if self._is_faulty_coordinate(xc, yc):
-                clusters[(xi, yi)] = None
-                faulty_indexes.append((xi, yi))
+        position = np.array(list(clusters.keys()))
+        (xi_max,yi_max) = position.T.max(axis = 1)
+        pos_temp = (position.T*[[self.step_x],[self.step_y]]+[[self.min_x],[self.min_y]])
+        if len(self.faulty_region_.shape) > 1:
+            length=self.faulty_region_.shape[1]
+            flag = np.array(
+                [
+                    (pos_temp > self.faulty_region_[:2].reshape(1,2,length).T).T.all(axis=1),
+                    (pos_temp < self.faulty_region_[2:].reshape(1,2,length).T).T.all(axis=1)
+                ]
+            ).all(axis = 0).any(axis = 1)
+            for i in range(len(flag)):
+                if(flag[i]):
+                    clusters[tuple(position[i])] = None
+                    faulty_indexes.append(tuple(position[i]))
+        del pos_temp
 
         def get_nearest(start, dx, dy):
-            (x, y) = start
-            x += dx
-            y += dy
-            while (x >= 0 and x <= xi_max and
-                   y >= 0 and y <= yi_max):
-                if clusters[(x, y)] is not None:
-                    return (abs(x-start[0])+abs(y-start[0]), median(clusters[(x,y)]))
-                x += dx
-                y += dy
+            inputs = np.array(start)
+            inputs += [dx,dy]
+            while ((inputs >= 0).all() and (inputs <= [xi_max,yi_max]).all()):
+                if clusters[tuple(inputs)] is not None:
+                    return (abs(inputs-start[0]).sum(), median(clusters[tuple(inputs)]))
+                inputs += [dx,dy]
             return None
 
         def interp_weighted(lower, higher):
@@ -1987,7 +1990,13 @@ def arc_points(cx, cy, r, start_angle, span):
         points.append((x,y))
 
     return points
-
+    
+def convert_float(data):
+    toFloat=float(data)
+    if np.isinf(toFloat) or np.isnan(toFloat):
+        raise ValueError(f"convert error when convert string:{data} into float")
+    return toFloat
+    
 def coord_fallback(gcmd, name, parse, def_x, def_y, map=lambda v, d: v):
     param = gcmd.get(name, None)
     if param is not None:
